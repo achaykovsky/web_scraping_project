3a6a6e5501236f9712d21d70cac27b39dcb8dfac
Anna Chaykovsky <annachaykovsky@gmail.com>
2024-02-05 13:32:51 +0200

    added the main flow

---
+++ main.py
@@ -0,0 +1,14 @@
+import logging
+
+from scrapping import get_animal_names_and_adjectives, logger
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+if __name__ == "__main__":
+    wikipedia_url = "https://en.wikipedia.org/wiki/List_of_animal_names"
+
+    try:
+        get_animal_names_and_adjectives(wikipedia_url)
+    except Exception as e:
+        logger.error(f"An unexpected error occurred: {e}")
