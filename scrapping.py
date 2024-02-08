dcd5edd626125b0f8aca646a3c620324bfa13211
Anna Chaykovsky <annachaykovsky@gmail.com>
2024-02-08 13:54:30 +0200

    updated the column name for animal black updated the dictionary and extracted it for invalid words updated the function to allow comparisons

--- scrapping.py
+++ scrapping.py
@@ -5,8 +5,22 @@ from typing import List
 import requests
 from bs4 import BeautifulSoup
 
-COLLECTIVE_NOUN = 'collective noun'
-COLLATERAL_ADJECTIVE = 'collateral adjective'
+ANIMAL_COL_NAME = "animal"
+COLLATERAL_ADJECTIVE_COL_NAME = "collateral adjective"
+
+INVALID_WORDS_VOC = (
+    "or",
+    "for",
+    "on",
+    "a",
+    "of",
+    "list",
+    "male",
+    "female",
+    "young",
+    "also",
+    "see",
+)
 
 
 def fetch_url(url):
@@ -20,7 +34,7 @@ def fetch_url(url):
 
 def parse_page_content(response):
     if response and response.ok:
-        soup = BeautifulSoup(response.text, 'html.parser')
+        soup = BeautifulSoup(response.text, "html.parser")
         return soup
     return None
 
@@ -29,7 +43,7 @@ def find_column_indices(header_row):
     collective_noun_index = None
     collateral_adjective_index = None
     if header_row:
-        columns = header_row.find_all(['td', 'th'])
+        columns = header_row.find_all(["td", "th"])
 
         if len(columns) == 0:
             logger.warning("Row does not have enough columns. Skipping.")
@@ -37,9 +51,9 @@ def find_column_indices(header_row):
         for index, column in enumerate(columns):
             column_text = column.get_text(strip=True).lower()
 
-            if COLLECTIVE_NOUN in column_text:
+            if ANIMAL_COL_NAME in column_text:
                 collective_noun_index = index
-            elif COLLATERAL_ADJECTIVE in column_text:
+            elif COLLATERAL_ADJECTIVE_COL_NAME in column_text:
                 collateral_adjective_index = index
 
         return collective_noun_index, collateral_adjective_index
@@ -48,33 +62,48 @@ def find_column_indices(header_row):
 
 def strip_non_letters(input_string):
     # Use a regex to remove anything that is not a letter
-    stripped_string = re.sub(r'[^a-zA-Z]', ',', input_string).replace(' ', '').rstrip()
+    stripped_string = re.sub(r"[^a-zA-Z]", ",", input_string).replace(" ", "").rstrip()
     return stripped_string
 
 
 def extract_animal_info(soup):
     try:
         if soup:
-            tables = soup.find_all('table', {'class': 'wikitable'})
+            tables = soup.find_all("table", {"class": "wikitable"})
 
             for table in tables:
-                rows = table.find_all('tr')
+                rows = table.find_all("tr")
 
-                collective_noun_index, collateral_adjective_index = find_column_indices(rows[0])
+                collective_noun_index, collateral_adjective_index = find_column_indices(
+                    rows[0]
+                )
+
+                if collective_noun_index is None or collateral_adjective_index is None:
+                    continue
 
                 for row in rows[1:]:
-                    columns = row.find_all(['td', 'th'])
+                    columns = row.find_all(["td", "th"])
 
                     if len(columns) >= 2:
                         # there are rows that are grouped and are not relevant for the task,
                         # it's a validation that we have at least 2 columns
-                        text_from_animal_column = columns[collective_noun_index].get_text(strip=True)
-                        animals_name = strip_non_letters(text_from_animal_column).split(',')
+                        text_from_animal_column = columns[
+                            collective_noun_index
+                        ].get_text(strip=True)
+                        animals_name = strip_non_letters(text_from_animal_column).split(
+                            ","
+                        )
                         animals_list = remove_invalid_words(animals_name)
 
-                        text_from_adjective_column = columns[collateral_adjective_index].get_text(strip=True)
-                        collateral_adjectives = strip_non_letters(text_from_adjective_column).split(',')
-                        collateral_adjective_list = remove_invalid_words(collateral_adjectives)
+                        text_from_adjective_column = columns[
+                            collateral_adjective_index
+                        ].get_text(strip=True)
+                        collateral_adjectives = strip_non_letters(
+                            text_from_adjective_column
+                        ).split(",")
+                        collateral_adjective_list = remove_invalid_words(
+                            collateral_adjectives
+                        )
 
                         log_animal_info(collateral_adjective_list, animals_list)
                     else:
@@ -92,15 +121,14 @@ def log_animal_info(collateral_adjective_list, animal_name_list):
 
 
 def validate_word_existence(word):
-    if word == '' or word is None:
+    if word == "" or word is None:
         return False
     return True
 
 
 def remove_invalid_words(words_list: List) -> List:
-    invalid_words = ('or', 'for', 'on', 'a', 'of')
     for word in words_list:
-        if word in invalid_words:
+        if word.lower() in INVALID_WORDS_VOC or not validate_word_existence(word):
             words_list.remove(word)
     return words_list
 
